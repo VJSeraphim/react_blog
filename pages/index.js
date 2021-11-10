60e7e9825c62b3931beac3627ddc37564de3ea17
VJS <minsung_96@naver.com>
2021-11-10 23:56:43 +0900

    Components-Post #1

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'React Test', excerpt: 'React Testing'},
-  { title: 'React Test with tailwind', excerpt: 'React Testing wiat tailwind'},
-]
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +12,7 @@ export default function Home() {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, i) => (
-            <PostCard post={post} key={post.title}/>
+            <PostCard post={post.node} key={post.title}/>
           ))}
         </div>  
       </div>
@@ -28,4 +24,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: { posts }
+  }
 }
--- services/index.js
+++ services/index.js
@@ -2,37 +2,38 @@ import { request, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
-export const getPosts  = async () => {
+export const getPosts = async () => {
     const query = gql`
         query MyQuery {
             postsConnection {
-            edges {
-                node {
-                author {
-                    bio
-                    id
-                    name
-                    photo {
-                    url
+                edges {
+                    node {
+                        author {
+                            bio
+                            id
+                            name
+                            photo {
+                            url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                            url
+                        }
+                        categories {
+                            name
+                            slug
+                        }
                     }
                 }
-                createdAt
-                slug
-                title
-                excerpt
-                featuredImage {
-                    url
-                }
-                categories {
-                    name
-                    slug
-                }
-                }
-            }
             }
         }
     `
 
-    const results = await request(graphqlAPI, query)
+    const result = await request(graphqlAPI, query)
       
+    return result.postsConnection.edges
 }
