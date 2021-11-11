4817b7ce7b27636a51686d6d3edfe54beaa35ee0
VJS <minsung_96@naver.com>
2021-11-11 23:59:34 +0900

    Widget #1

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
 const PostWidget = () => {
+    const [relatedPosts, setRelatedPosts] = useState([])
+
+    useEffect(() => {
+
+    }, [])
+    
     return (
         <div>
             
