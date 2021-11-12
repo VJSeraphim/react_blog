760c1f91a88f2189c1c64e32d06ec5221ecd990d
VJS <minsung_96@naver.com>
2021-11-13 00:00:01 +0900

    Widget #2

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,16 +2,39 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget = ({ categories, slug }) => {
     const [relatedPosts, setRelatedPosts] = useState([])
 
     useEffect(() => {
-
-    }, [])
+        if(slug) {
+            getSimilarPosts(categories, slug)
+            .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+            .then((result) => setRelatedPosts(result))
+        }
+    }, [slug])
     
     return (
-        <div>
-            
+        <div className="bg-white shadow-lg rounded-lg p-8 mb-8 ">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4 ">
+               {slug ? 'You May Also Like...' : "Recent Posts..."} 
+            </h3>
+            {relatedPosts.map((post) => (
+                <div key={post.title} className="flex items-center w-full mb-4">
+                    <div className="w-16 flex-none">
+                        <img 
+                            src={post.featuredImage.url}
+                            alt={post.title}
+                            height="60px"
+                            width="60px"
+                            className="align-middle rounded-full"
+                        />
+                    </div>    
+                </div>
+            ))}
         </div>
     )
 }
