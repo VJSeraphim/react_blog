81d12c7849cc569779708488f81607962cc6ecdc
VJS <minsung_96@naver.com>
2021-11-14 23:58:48 +0900

    Debug #1

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,7 +18,7 @@ const PostWidget = ({ categories, slug }) => {
     }, [slug])
     
     return (
-        <div className="bg-white shadow-lg rounded-lg p-8 mb-8 ">
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8 ">
             <h3 className="text-xl mb-8 font-semibold border-b pb-4 ">
                {slug ? 'You May Also Like...' : "Recent Posts..."} 
             </h3>
