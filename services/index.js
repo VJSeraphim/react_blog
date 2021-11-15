e1579a72ba6aedcc0b657bc954b9b25e12396d91
VJS <minsung_96@naver.com>
2021-11-15 23:52:41 +0900

    Post Details #2

--- services/index.js
+++ services/index.js
@@ -38,6 +38,42 @@ export const getPosts = async () => {
     return result.postsConnection.edges
 }
 
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+            post(where: {slug: slug}){
+                author {
+                    bio
+                    id
+                    name
+                    photo {
+                    url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }
+        }
+
+    `
+
+    const result = await request(graphqlAPI, query, { slug })
+      
+    return result.post
+}
+
 export const getRecentPosts = async () => {
     const query = gql`
         query GetPostDetails() {
@@ -77,7 +113,7 @@ export const getSimilarPosts = async() => {
         }
     `
 
-    const result = await request(graphqlAPI, query)
+    const result = await request(graphqlAPI, query, { categories, slug })
       
     return result.posts
 }
