6633659099880cc85d0cae481e8b7b8e840f857a
VJS <minsung_96@naver.com>
2021-11-19 23:58:30 +0900

    Author #1

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Author = () => {
+const Author = ({ author }) => {
     return (
         <div>
-            
+            <img 
+                src={author.photo.url}
+                alt={author.name}
+                height="100px"
+                width="100px"
+                className="align-middle rounded-full"
+            />
         </div>
     )
 }
